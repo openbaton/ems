8ca74118d2cb359d498fc5575676139dbcab361a
lorenzo.tomasini <lorenzo.tomasini@fokus.fraunhofer.de>
2015-07-23 19:09:45 +0200

    improved receiver

--- receiver/Receiver.py
+++ receiver/Receiver.py
@@ -12,23 +12,29 @@ class EMSReceiver(stomp.ConnectionListener):
 
     def __init__(self, conn):
         self.conn = conn
+        self.scripts = True
 
     def on_error(self, headers, message):
         log.info('received an error %s' % message)
 
     def on_message(self, headers, message):
-        log.info('received a message %s' % message)
-        proc = subprocess.Popen(message.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        status = proc.wait()
-        out, err = proc.communicate()
+        if self.scripts:
+            f = open("/opt/")
+
+            self.scripts = False
+        else:
+            log.info('received a message %s' % message)
+            proc = subprocess.Popen(message.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            status = proc.wait()
+            out, err = proc.communicate()
 
 
-        resp = {
-            'output': out,
-            'err': err,
-            'status': status
-        }
-        json_str = json.dumps(resp)
-        log.info("answer is: " + json_str)
-        self.conn.send(body=json_str, destination='/queue/ems-vnfm-actions')
+            resp = {
+                'output': out,
+                'err': err,
+                'status': status
+            }
+            json_str = json.dumps(resp)
+            log.info("answer is: " + json_str)
+            self.conn.send(body=json_str, destination='/queue/ems-vnfm-actions')
 
