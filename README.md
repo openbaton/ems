c465466b1e74b6bd197bd982bdad3f584a71f61b
Oleksander Gozman <oleksander.gozman@fokus.fraunhofer.de>
2015-12-17 20:27:44 +0100

    new ems to master

--- README.md
+++ README.md
@@ -5,18 +5,22 @@
 ### Prerequisites
 
 * python 2.7 
-* stomp.py
+* pika 0.10.0
+* pythonGit
+* git
 
 ### Usage
 
-```bash
-cd ..
-python ems
-```
+sudo service ems start to start
+sudo service ems stop to stop
 
 ### conf.ini
 
-Inside the folder etc there is a configuration file, conf.ini, where:
+Inside the folder etc there is a configuration file, conf.ini, where parameteres are stored
 
-* *orch_ip*: the ip of the NFVO
-* *orch_port*: the port of NFVO
+
+
+Element Management System works by getting the commands from orchestrator through the manager. It is supposed to control the virtual network funktion. It receives its commands throught the broker(RabbitMQ server).
+After the EMS starts, it connects to rabbitemq-server, subscribes to the queue with its name and sends register message to the specific register-queue on the server.
+EMS waits for the messages in json-format and executes them on arrival. It can clone scripts from repository, update them and execute with the arguments that were sent with the message. 
+
--- __main__.py
+++ __main__.py
@@ -1,58 +1,104 @@
+# Copyright (c) 2015 Fraunhofer FOKUS. All rights reserved.
+ #
+ # Licensed under the Apache License, Version 2.0 (the "License");
+ # you may not use this file except in compliance with the License.
+ # You may obtain a copy of the License at
+ #
+ #     http://www.apache.org/licenses/LICENSE-2.0
+ #
+ # Unless required by applicable law or agreed to in writing, software
+ # distributed under the License is distributed on an "AS IS" BASIS,
+ # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ # See the License for the specific language governing permissions and
+ # limitations under the License.
+ #
+
+
+
+#!/usr/bin/env python
 import argparse
+import threading
+import os
 import sys
 import time
 import pkg_resources
 import logging
-import stomp
 import ConfigParser
+import pika
+from receiver import on_message
+from utils import get_map
+__author__ = 'ogo'
 
-from receiver.Receiver import EMSReceiver
-from utils.Utils import get_map
-
-__author__ = 'lto'
 
 log = logging.getLogger(__name__)
-usage = "\nThis is the template module for the platform Crenation"
 
-LEVELS = {'0': logging.DEBUG,
-          '1': logging.INFO,
-          '2': logging.WARNING,
-          '3': logging.ERROR,
-          '4': logging.CRITICAL,
-          }
+
+def on_request(ch, method, props, body):
+    n = body
+    response = on_message(body)
+    ch.basic_publish(exchange='',
+                     routing_key=props.reply_to,
+                     properties=pika.BasicProperties(correlation_id = \
+                                                     props.correlation_id, content_type='text/plain'),
+                     body=str(response))
+    ch.basic_ack(delivery_tag = method.delivery_tag)
+def thread_function(ch, method, properties, body):
+        threading.Thread(target=on_request, args=(ch, method, properties, body)).start()
 
 
 def main():
-    config_file_name = pkg_resources.resource_filename('etc', 'conf.ini')
+    config_file_name = "/etc/openbaton/ems/conf.ini"
     log.debug(config_file_name)
     config = ConfigParser.ConfigParser()
-    config.read(config_file_name)
-    _map = get_map(section='ems', config=config)
-    conn = stomp.Connection(host_and_ports=[(_map.get("orch_ip"), int(_map.get("orch_port")))])
-    conn.set_listener('ems_receiver', EMSReceiver(conn=conn))
-    conn.start()
-    conn.connect()
+    config.read(config_file_name) #read config file
+    _map = get_map(section='ems', config=config) #get the data from map
+    queue_type = _map.get("type") #get type of the queue
+    hostname = _map.get("hostname")
+    username = _map.get("username")
+    password = _map.get("password")
+    autodel = _map.get("autodelete")
+    heartbeat= _map.get("heartbeat")
+    exchange_name = _map.get("exchange")
+    queuedel = True
+    if autodel == 'false':
+        queuedel = False
+    if not heartbeat:
+        heartbeat = '60'
+    if not exchange_name:
+	exchange_name = 'openbaton-exchange'
 
-    conn.subscribe(destination='/queue/vnfm-ems-actions', id=1, ack='auto')
 
-    try:
-        while True:
-            time.sleep(10000)
-    except KeyboardInterrupt:
-        conn.disconnect()
-        sys.exit(0)
 
+
+    rabbit_credentials = pika.PlainCredentials(username,password)
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=_map.get("orch_ip"), credentials = rabbit_credentials, heartbeat_interval=int(heartbeat)))
+
+    channel = connection.channel()
+
+
+    channel.exchange_declare(exchange=exchange_name, type="topic", durable=True)
+    channel.queue_declare(queue='ems.%s.register'%queue_type, auto_delete=queuedel)
+    channel.queue_declare(queue='vnfm.%s.actions'%hostname, auto_delete=queuedel)
+    channel.queue_bind(exchange=exchange_name, queue='ems.%s.register'%queue_type)
+    channel.queue_bind(exchange=exchange_name, queue='vnfm.%s.actions'%hostname)
+    channel.basic_publish(exchange='',routing_key='ems.generic.register',properties=pika.BasicProperties(content_type='text/plain'), body='{"hostname":"%s"}' % hostname)
+
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(thread_function, queue='vnfm.%s.actions'%hostname)
+    print "Waiting for actions"
+    channel.start_consuming()
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Template Module', usage=usage)
-    parser.add_argument('-l', '--log-level',
-                        help='possible values are [0, 1, 2, 3, 4] where 0 is the maximum and 4 is lowest')
+    logging_dir='/var/log/openbaton/'
 
+
+    if not os.path.exists(logging_dir):
+    	os.makedirs(logging_dir)
     if len(sys.argv) > 1:
         args = vars(parser.parse_args(sys.argv[1:]))
         log_level = args.get('log_level')
         level = LEVELS.get(log_level)
-        logging.basicConfig(level=level)
+        logging.basicConfig(filename=logging_dir+'/ems-receiver.log', level=level)
     else:
-        logging.basicConfig(level=logging.INFO)
+        logging.basicConfig(filename=logging_dir+'/ems-receiver.log', level=logging.DEBUG)
     main()
