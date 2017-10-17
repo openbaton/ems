06940ab36ae1b5ab2cdff975daa10cb72e8f8063
ogozman <oleksandr.gozman@fokus.fraunhofer.de>
2017-10-17 15:33:09 +0200

    increase version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name="openbaton-ems",
-    version="1.0.1",
+    version="1.0.2",
     author="Openbaton",
     author_email="dev@openbaton.org",
     description="Openbaton generic EMS",
